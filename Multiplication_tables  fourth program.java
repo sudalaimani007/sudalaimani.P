5de7f24310c52c97e4959679b8126e16599b041b
sudalaimani007 <161324417+sudalaimani007@users.noreply.github.com>
2024-02-26 20:37:42 +0530

    Add files via upload

---
+++ Multiplication_tables  fourth program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Multiplication_tables {
+
+    public static void main(String[] args) {
+        
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number :");
+        number = sc.nextInt();
+
+
+    }
+    
+}
